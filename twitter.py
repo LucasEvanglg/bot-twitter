b7bde1fba14f55b42af52e007dbe476bb682bd0f
LucasEvanglg <lucassiqueiraevangelista@gmail.com>
2021-06-04 15:36:47 -0300

    create API

--- twitter.py
+++ twitter.py
@@ -6,6 +6,14 @@ import time
 auth = tweepy.OAuthHandler('nN6gfj68xdG6lt8KAVT3yLPdt','oVWvcf4mGgSng7uS1NzK7IkFNosornGV7EhFLTdnEoTNSEijnP')
 auth.set_access_token('1372545170005184513-wptOvtcnODuT9BlWGWgJwZaarb4UB1','4O97ppmjOBmja1TAmeSsKLQvQDTpHSucLGYfQweRSAQVU')
 
+#Create api#
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+search = 'Anapolis','Anápolis'
+numeroDeTweets = 1000
+
 
 
 
