0661e00fd2514a88f320a8bbe7ca98a13630d8e7
[LucasEvanglg] <lucassiqueiraevangelista@gmail.com>
2021-06-04 15:10:50 -0300

    authenticador do twitter

---
+++ twitter.py
@@ -0,0 +1,11 @@
+import tweepy
+import time
+
+
+#authenticador do twitter#
+auth = tweepy.OAuthHandler('nN6gfj68xdG6lt8KAVT3yLPdt','oVWvcf4mGgSng7uS1NzK7IkFNosornGV7EhFLTdnEoTNSEijnP')
+auth.set_access_token('1372545170005184513-wptOvtcnODuT9BlWGWgJwZaarb4UB1','4O97ppmjOBmja1TAmeSsKLQvQDTpHSucLGYfQweRSAQVU')
+
+
+
+
